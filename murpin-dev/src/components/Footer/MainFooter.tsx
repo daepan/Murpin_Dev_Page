7c504c1da27f12c2ca241871df7e6b4cc21514e0
murpin <qw04011@gmail.com>
2022-07-16 15:45:34 +0900

    style : Write MainFooter wireframe

--- murpin-dev/src/components/Footer/MainFooter.tsx
+++ murpin-dev/src/components/Footer/MainFooter.tsx
@@ -2,7 +2,14 @@ import styled from 'styled-components';
 
 
 const MainFooter = () => {
-  return(<StyledMainFooter></StyledMainFooter>);
+  return(<StyledMainFooter>
+    <FooterTitle>Director : KIMMurPin</FooterTitle>
+    <FooterContent>
+        <FooterFacebookLink>FaceBook</FooterFacebookLink>
+        <FooterBlogLink>Blog</FooterBlogLink>
+        <FooterGoogleEmailLink>Google</FooterGoogleEmailLink>
+    </FooterContent>
+  </StyledMainFooter>);
 };
 
 
@@ -10,6 +17,27 @@ export default MainFooter;
 
 
 const StyledMainFooter = styled.div`
-
-
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    position : relative;
+    transform : translateY(-100%);
 `
+const FooterTitle = styled.div`
+    font-size: 27px;
+`
+const FooterContent = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between; 
+    margin-top: 20px;
+`
+const FooterLink = styled.div`
+    margin-left: 5px;
+`
+const FooterFacebookLink = styled(FooterLink)`
+`
+const FooterBlogLink =  styled(FooterLink)``
+
+const FooterGoogleEmailLink =  styled(FooterLink)``
