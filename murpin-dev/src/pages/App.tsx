42af8c927a3dcb3577eeabeb76b40bc11f540d4f
daepan <qw04011@gmail.com>
2022-07-17 23:04:09 +0900

    fix : setting font-face, fix content height

--- murpin-dev/src/pages/App.tsx
+++ murpin-dev/src/pages/App.tsx
@@ -1,18 +1,26 @@
 import MainHeader from "../components/Header/MainHeader";
 import MainFooter from "../components/Footer/MainFooter";
 import styled from "styled-components";
+import './App.css';
 
 function App() {
   return (
-   <div>
-    <MainContent/>
+   <Main>
+    <MainHeader/>
+      <MainContent>
+        
+      </MainContent>
     <MainFooter/>
-   </div>
+   </Main>
   );
 }
 
 const MainContent = styled.div`
   display: flex;
-  height: 100vh;
+  min-height: calc(100vh - 200px - 45px);
 `
+
+const Main = styled.div`
+`
+
 export default App;
