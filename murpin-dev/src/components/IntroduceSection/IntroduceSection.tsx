1aceee6b0f8ed931f5c32f1c98ec0c48c4842e72
murpin <qw04011@gmail.com>
2022-07-19 00:17:20 +0900

    chore : add IntroduceSection

---
+++ murpin-dev/src/components/IntroduceSection/IntroduceSection.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+
+const IntroduceSection = () => {
+  return(<StyledIntroduceSection></StyledIntroduceSection>);
+};
+
+
+export default IntroduceSection;
+
+
+const StyledIntroduceSection = styled.div`
+
+
+`
