eade56fe756693d1d57233f2ee7d4831b18b246f
murpin <qw04011@gmail.com>
2022-07-19 17:33:47 +0900

    chore: add ProfileSection

---
+++ murpin-dev/src/components/ProfileSection/ProfileSection.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+
+const ProfileSection = () => {
+  return(<StyledProfileSection></StyledProfileSection>);
+};
+
+
+export default ProfileSection;
+
+
+const StyledProfileSection = styled.div`
+
+
+`
