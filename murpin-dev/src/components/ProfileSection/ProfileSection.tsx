b341e91f94ddc1bf8d0098aa598fe1c7c7fed0dd
murpin <qw04011@gmail.com>
2022-07-28 18:04:35 +0900

    test : test three code

--- murpin-dev/src/components/ProfileSection/ProfileSection.tsx
+++ murpin-dev/src/components/ProfileSection/ProfileSection.tsx
@@ -1,15 +1,26 @@
 import React, { useRef} from 'react';
 import { useIntersection } from 'use-intersection';
 import styled from 'styled-components';
-
+import { Canvas } from '@react-three/fiber';
 
 const ProfileSection = () => {
   const profileRef = useRef<HTMLDivElement>(null);
   const profileIntersecting = useIntersection(profileRef);
 
+
   return(<StyledProfileSection
-  ref={profileRef}>
-    {profileIntersecting ? 'invisibleProfile': 'visibleProfile'}
+  >
+    <div ref={profileRef}>{profileIntersecting ? 
+    <Canvas>
+      <pointLight position={[10, 10, 10]} />
+    <mesh>
+      <sphereBufferGeometry />
+      <meshStandardMaterial color="hotpink" />
+    </mesh>
+    </Canvas>
+    : 'visibleProfile'}</div>
+   
+
   </StyledProfileSection>);
 };
 
