9f5324392a4123fb47a29ba27260ae486da007e2
murpin <qw04011@gmail.com>
2022-07-29 23:32:00 +0900

    chore : add scenejs effect

---
+++ murpin-dev/src/utils/animation/typing.ts
@@ -0,0 +1,24 @@
+import Scene from "scenejs";
+
+const typing = new Scene({
+  ".class": {
+    0: "left: 0px; top: 0px; transform: translate(0px);",
+    1: {
+      "left": "100px",
+      "top": "0px",
+      transform: "translate(50px)",
+    },
+    2: {
+      "left": "200px",
+      "top": "100px",
+      transform: {
+        translate: "100px",
+      },
+    }
+  }
+}, {
+  selector: true,
+  easing: "ease-in-out",
+}).play();
+
+export default typing;
